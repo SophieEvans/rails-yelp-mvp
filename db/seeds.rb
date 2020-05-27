8f1e1af03cca49ffc1a5dc08ad30820251c6bab1
Sophie Evans <emailsophiep@gmail.com>
2020-05-27 12:59:27 -0300

    adding files for incomplete Yelp challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+categories = %w[chinese italian japanese french belgian]
+
+puts 'Creating 10 fake posts...'
+10.times do
+  article = Restaurant.new(
+    name: "#{Faker::Movies::StarWars.character}'s #{Faker::Restaurant.name}",
+    address: Faker::Address.street_address,
+    category: categories.sample
+  )
+  article.save!
+end
+puts 'Finished!'
